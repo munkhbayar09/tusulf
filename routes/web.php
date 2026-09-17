b0786a6725a32f53103ad929692b14731e287875
Munkhbayar <ba.bayaraa09@gmail.com>
2026-09-17 18:50:17 +0800

    Add temporary debug route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/debug-check', function () {
+    return response()->json([
+        'app_debug' => config('app.debug'),
+        'app_env' => config('app.env'),
+        'app_url' => config('app.url'),
+        'db_connection' => config('database.default'),
+        'user_count' => \App\Models\User::count(),
+        'is_authenticated' => auth()->check(),
+    ]);
+});
