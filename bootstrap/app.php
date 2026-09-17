273653d5194846501a1b19edfc267659d40e6faa
Munkhbayar <ba.bayaraa09@gmail.com>
2026-09-17 18:18:35 +0800

    Log all exceptions for debugging

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,4 +18,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $exceptions->shouldRenderJsonWhen(
             fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
         );
+        $exceptions->report(function (\Throwable $e) {
+            \Log::error($e->getMessage(), ['exception' => get_class($e)]);
+        });
     })->create();
